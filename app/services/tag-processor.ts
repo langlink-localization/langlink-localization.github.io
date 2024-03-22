fdc4784595787cc3cf93f15baf6f82589dfef5c0
warecast <tk4900@gmail.com>
2024-03-22 15:13:10 +0800

    更改折叠tag的逻辑

--- app/services/tag-processor.ts
+++ app/services/tag-processor.ts
@@ -5,7 +5,7 @@ interface tagConvertedResult {
 
 export const tagProcessor = (oldText: string): tagConvertedResult => {
   const tagRegex =
-    /<(\/?)(g|x|bx|ex|bpt|ept|ph|it|mrk|cp|pc|sc|ec|sm|em|a|br|mq|key|span (?!class))([^>]*)>/gm;
+    /<(\/?)(g|x|bx|ex|bpt|ept|ph|it|mrk|cp|pc|sc|ec|sm|em|a|br|mq:[a-z\-]+|key|span (?!class))([^>]*)>/gm;
 
   let grayedString = oldText.replace(
     tagRegex,
@@ -14,7 +14,7 @@ export const tagProcessor = (oldText: string): tagConvertedResult => {
 
   let shortenedString = oldText.replace(
     tagRegex,
-    `<span class="text-fuchsia-600">⬣</span>`,
+    `<span class="text-fuchsia-600">&lt;$1$2&gt;</span>`,
   );
 
   return { grayedString, shortenedString };
